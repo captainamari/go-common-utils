098132175f247a883ebd91b68a51c04ccaffda96
captainamari <pharahamari@protonmail.com>
2022-11-24 21:00:51 +0800

    add unicodedecode

--- utils/string.go
+++ utils/string.go
@@ -2,7 +2,9 @@ package utils
 
 import (
 	"math/rand"
+	"strings"
 	"time"
+	"unicode/utf8"
 )
 
 // GetRandomString 生成随机字符串
@@ -15,4 +17,21 @@ func GetRandomString(length int) string {
 		result = append(result, bytes[r.Intn(len(bytes))])
 	}
 	return string(result)
+}
+
+// UnicodeDecodeString 解码unicode
+func UnicodeDecodeString(s string) string {
+	if s == "" {
+		return s
+	}
+	newStr := make([]string, 0)
+	for i := 0; i < len(s); {
+		r, n := utf8.DecodeRuneInString(s[i:])
+		newStr = append(newStr, fmt.Sprintf("%c", r))
+		i += n
+	}
+	if len(newStr) == 0 {
+		return s
+	}
+	return strings.Join(newStr, "")
 }
