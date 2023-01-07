612522d4ef056ac07550fc0adfc8b32b64af0b5d
captainamari <pharahamari@protonmail.com>
2023-01-07 15:18:04 +0800

    add camel underline exchange

--- utils/string.go
+++ utils/string.go
@@ -4,7 +4,6 @@ import (
 	"math/rand"
 	"strings"
 	"time"
-	"unicode/utf8"
 )
 
 // GetRandomString 生成随机字符串
@@ -19,19 +18,72 @@ func GetRandomString(length int) string {
 	return string(result)
 }
 
-// UnicodeDecodeString 解码unicode
-func UnicodeDecodeString(s string) string {
-	if s == "" {
-		return s
+// CamelToUnderline AbAb to ab_ab , AbBB to aa_bb
+func CamelToUnderline(s string) string {
+	//先将连续的大写转换为第一个大写，后面小写，如果后面大写接的小写，则将最近的小写转大写
+	num := len(s)
+	tempData := make([]string, 0, len(s))
+	for i := 0; i < num; i++ {
+		d := s[i]
+		tempData = append(tempData, string(d))
+		if d >= 'A' && d <= 'Z' {
+			n := 0
+			i = i + 1
+			for ; i < num; i++ {
+				d2 := s[i]
+				if d2 >= 'A' && d2 <= 'Z' {
+					n = n + 1
+					tempData = append(tempData, strings.ToLower(string(d2)))
+				} else {
+					//表示是多个大写后的第一个小写
+					if n > 0 {
+						tempData = append(tempData, strings.ToUpper(string(d2)))
+					} else {
+						tempData = append(tempData, string(d2))
+					}
+					break
+				}
+			}
+		}
 	}
-	newStr := make([]string, 0)
-	for i := 0; i < len(s); {
-		r, n := utf8.DecodeRuneInString(s[i:])
-		newStr = append(newStr, fmt.Sprintf("%c", r))
-		i += n
+	s = strings.Join(tempData, "")
+	data := make([]byte, 0, len(s)*2)
+	j := false
+	for i := 0; i < num; i++ {
+		d := s[i]
+		if i > 0 && d >= 'A' && d <= 'Z' && j {
+			data = append(data, '_')
+		}
+		if d != '_' {
+			j = true
+		}
+		data = append(data, d)
 	}
-	if len(newStr) == 0 {
-		return s
+	res := strings.ToLower(string(data[:]))
+	return res
+}
+
+// UnderlineToCamel ab_ab to AbAb
+func UnderlineToCamel(s string) string {
+	data := make([]byte, 0, len(s))
+	j := false
+	k := false
+	num := len(s) - 1
+	for i := 0; i <= num; i++ {
+		d := s[i]
+		if k == false && d >= 'A' && d <= 'Z' {
+			k = true
+		}
+		if d >= 'a' && d <= 'z' && (j || k == false) {
+			d = d - 32
+			j = false
+			k = true
+		}
+		if k && d == '_' && num > i && s[i+1] >= 'a' && s[i+1] <= 'z' {
+			j = true
+			continue
+		}
+		data = append(data, d)
 	}
-	return strings.Join(newStr, "")
+	return string(data[:])
 }
